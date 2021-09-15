59b2086f52a91f4788cf26aa132f3cdbb951fed5
tanjamilisavljevic <70627722+tanjamilisavljevic@users.noreply.github.com>
2021-09-15 10:02:36 +0200

    Prepare workspace

---
+++ src/Components/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Questions from "./Questions";
+
+function Layout(props) {
+    return (
+        <div>
+            <h1> The IMPOSSIBLE Quiz</h1>
+            <Questions/>
+        </div>
+    );
+}
+
+export default Layout;
---
+++ src/Components/Questions.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Questions(props) {
+    return (
+        <div>
+            <p> blab la bla </p>
+            <input type='text' className='answer' id='answer'/>
+            <br/>
+            <input type='submit' className='submit' id='submit' value='Check my answer'/>
+        </div>
+    );
+}
+
+export default Questions;
